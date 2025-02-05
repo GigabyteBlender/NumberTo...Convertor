f09943dc1b5f679c69086d8003ff815165c1b6aa
PythonLibraryEnthusiast <dan.pertu@gmail.com>
2025-02-05 19:03:30 +0000

    update main.py

--- main.py
+++ main.py
@@ -1,14 +1,61 @@
-class ConvertToRoman:
-    def __init__(self):
-        self.characters = {1:'I',5:'V',10:'X',50:'L',100:'C',1000:'M'}
+class ConvertToDecimal:
+
+    def from_roman(self, roman):
+        roman_to_int = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        decimal = 0
+        prev_value = 0
+
+        for char in reversed(roman):
+            value = roman_to_int[char]
+            if value < prev_value:
+                decimal -= value
+            else:
+                decimal += value
+            prev_value = value
+
+        return decimal
+
     def Convert(self):
-        decimal = int(input('Enter the number to convert'))
-        decimal = str(decimal)
+        roman = input('Enter the Roman numeral to convert: ')
+        print(self.from_roman(roman))
+
+class ConvertToRoman:
+
+    def to_roman(self, num):
+
+        val = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1
+            ]
+        syb = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+            ]
         
-        for x in enumerate(decimal):
-            print(x)
+        roman_num = ''
+        i = 0
+        
+        while num > 0:
+            for _ in range(num // val[i]):
+                roman_num += syb[i]
+                num -= val[i]
+            i += 1
+        return roman_num
+
+    def Convert(self):
+        decimal = int(input('Enter the number to convert: '))
+        print(self.to_roman(decimal))
 
 if __name__ == '__main__':
     main = ConvertToRoman()
     main.Convert()
-    
+
+    main = ConvertToDecimal()
+    main.Convert()
